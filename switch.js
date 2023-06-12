c96b62d269de1c7e11dbc23d2e8ecbe1a0a5db5d
arslan <iamarslanbaloch@gmail.com>
2023-06-12 12:06:00 +0500

    switch_added

---
+++ switch.js
@@ -0,0 +1,19 @@
+
+
+
+let number = +prompt('Enter the number','');
+
+switch(number)
+{
+    case(0):
+    alert(0)
+
+    case(1):
+    alert(1)
+
+    case(2):
+    case(3):
+    alert('2,3')
+}
+
+
