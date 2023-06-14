fa5ce120facf999bf5f61a39409a760734e16b85
arslan <iamarslanbaloch@gmail.com>
2023-06-14 22:02:25 +0500

    symbol-added

---
+++ symbol.js
@@ -0,0 +1,20 @@
+let id1 = Symbol();
+
+let id2 = Symbol("id");
+
+let id3 = Symbol("id");
+let id4 = Symbol("id");
+
+alert(id3 == id4);
+
+
+let id = Symbol("id");
+alert(id);
+
+let id5 = Symbol("id");
+alert(id5.toString());
+
+
+let id6 = Symbol("id");
+alert(id6.description);
+
