1a606a743854d35942f79cf17192510efad6fd21
arslan <iamarslanbaloch@gmail.com>
2023-06-22 20:20:26 +0500

    recursion added

---
+++ recurson.js
@@ -0,0 +1,195 @@
+
+
+function pow(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  alert( pow(2, 3) );
+
+
+
+
+
+let company = {
+    sales: [{
+      name: 'John',
+      salary: 1000
+    }, {
+      name: 'Alice',
+      salary: 1600
+    }],
+  
+    development: {
+      sites: [{
+        name: 'Peter',
+        salary: 2000
+      }, {
+        name: 'Alex',
+        salary: 1800
+      }],
+  
+      internals: [{
+        name: 'Jack',
+        salary: 1300
+      }]
+    }
+  };
+
+
+
+
+  let company1 = { 
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+    development: {
+      sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+      internals: [{name: 'Jack', salary: 1300}]
+    }
+  };
+  
+
+  function sumSalaries(department) {
+    if (Array.isArray(department)) {
+      return department.reduce((prev, current) => prev + current.salary, 0);
+    } else {
+      let sum = 0;
+      for (let subdep of Object.values(department)) {
+        sum += sumSalaries(subdep);
+      }
+      return sum;
+    }
+  }
+
+
+  let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+
+
+  let list1 = { value: 1 };
+list.next = { value: 2 };
+list.next.next = { value: 3 };
+list.next.next.next = { value: 4 };
+list.next.next.next.next = null;
+
+
+function sumTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  alert( sumTo(100) );
+
+
+  function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+  }
+  
+  alert( factorial(5) );
+
+
+
+  function fib(n) {
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  }
+  
+  alert( fib(3) );
+  alert( fib(7) ); 
+
+
+
+  let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+  
+  function printList(list) {
+    let tmp = list;
+  
+    while (tmp) {
+      alert(tmp.value);
+      tmp = tmp.next;
+    }
+  
+  }
+  
+  printList(list);
+
+
+  let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+  
+  function printList(list) {
+  
+    alert(list.value); 
+  
+    if (list.next) {
+      printList(list.next);
+    }
+  
+  }
+  
+  printList(list);
+
+
+  let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+  
+  function printReverseList(list) {
+  
+    if (list.next) {
+      printReverseList(list.next);
+    }
+  
+    alert(list.value);
+  }
+  
+  printReverseList(list);
