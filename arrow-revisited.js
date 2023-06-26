11e085826659caf9e7c9b41976189456f38ce9ef
arslan <iamarslanbaloch@gmail.com>
2023-06-26 22:28:34 +0500

    arrow-revisited

---
+++ arrow-revisited.js
@@ -0,0 +1,26 @@
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+  
+    showList() {
+      this.students.forEach(
+        student => alert(this.title + ': ' + student)
+      );
+    }
+  };
+  
+  group.showList();
+
+
+  function defer(f, ms) {
+    return function() {
+      setTimeout(() => f.apply(this, arguments), ms);
+    };
+  }
+  
+  function sayHi(who) {
+    alert('Hello, ' + who);
+  }
+  
+  let sayHiDeferred = defer(sayHi, 2000);
+  sayHiDeferred("John");
