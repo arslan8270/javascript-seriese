fe592b563bd36a592639e6bf3e60cadc51446a5d
arslan <iamarslanbaloch@gmail.com>
2023-07-06 15:56:32 +0500

    url added

---
+++ url.js
@@ -0,0 +1,22 @@
+const myurl =  new URL('https://www.youtube.com/');
+
+console.log(myurl);
+
+console.log(myurl.toString());
+
+console.log(location);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.href);
+console.log(location.port);
+console.log(location.protocol);
+console.log(location.hash);
+console.log(location.search);
+
+
+
+
+
+
+
+
