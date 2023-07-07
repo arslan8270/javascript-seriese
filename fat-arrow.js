db03befb185b13cb4928290db392f149e6a892f4
arslan <iamarslanbaloch@gmail.com>
2023-07-07 15:31:57 +0500

    fat arrow added

---
+++ fat-arrow.js
@@ -0,0 +1,14 @@
+var sum = function(){
+    var a = 10;
+    var b  = 20;
+    return a+b;
+}
+console.log(sum());
+
+const summ =()=>{
+    let c = 5;
+    let d = 20;
+    return c+d;
+}
+
+console.log(summ())
